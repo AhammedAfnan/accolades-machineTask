232dc754f21325cad32036e0bf0de7cba3504c0c
afnan <afnanballoor44@gmail.com>
2025-02-19 15:56:25 +0530

    added Support Component, SectionThree Component

---
+++ src/app/components/Support.jsx
@@ -0,0 +1,22 @@
+export default function Support() {
+  return (
+    <div className="container mx-auto py-20">
+      <div>
+        <div className="">
+            <h1 className="text-[24px] leading-[26px] text-[Quicksand] text-[#121212] text-center">Formats We Support</h1>
+            <p className='text-[Inter-Regular] text-[16px] leading-[19.36px] text-[#999999] mt-4'>At Transfective DTP, we do more than just translate words; we transform your content into an engaging journey that captivates and resonates. Our services bridge linguistic and cultural divides, creating a compelling, informative, and immersive experience for your audience.</p>
+            <div className="flex space-x-10 justify-center mt-8">
+            <img src="/images/s8.png" alt="" />
+            <img src="/images/s7.png" alt="" />
+            <img src="/images/s6.png" alt="" />
+            <img src="/images/s5.png" alt="" />
+            <img src="/images/s4.png" alt="" />
+            <img src="/images/s3.png" alt="" />
+            <img src="/images/s2.png" alt="" />
+            <img src="/images/s1.png" alt="" />
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,16 @@
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Support from "./components/Support";
+import SectionThree from "./components/SectionThree";
 
 export default function Home() {
   return (
     <>
     <Navbar />
     <Hero />
+    <Support />
+    <SectionThree />
     <Footer />
     </>
   );
